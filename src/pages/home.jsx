49c4883ad2743af968bf059e1a5e52fef303c13b
alejandrohernandez1998 <alejandro.hernandezh@sansano.usm.cl>
2022-11-24 22:35:44 -0300

    informaciones

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,21 +5,23 @@ import NavBar from '../components/nav_bar'
 import Banner from '../components/banner'
 import { Card } from '@mui/material'
 import Postula from '../assets/banner_admision_postula.jpg'
+import ArancelDiurno from '../assets/diurno-arancel.jpg'
+import Admision from '../assets/consulta-adm2.jpg'
+import Simulacion from '../assets/680x360-simulador-pdt.jpg'
 
 
 function Button({children}) {
   return  <button  style={{
-    paddingRight : '80%',
+    background: '#04587d',
     position: 'relative',
-    left: '20px',
-    color: 'blue',
+    left: '40px',
+    color: 'white',
     border:'blue', 
-    borderradius:'10px',
     padding:'40px',
-    minheight:'40px', 
-    minwidth: '120px',
+    height:'40px', 
+    width: '360px',
     fontfamily: 'Rubik One',
-    fontsize: '40vw'
+    fontsize: '50vw'
   }
     }>{children}</button>;
 }
@@ -47,6 +49,39 @@ export const HomePage = () => {
         </div>
 
       </Card>
+      <h2>Informaciones</h2>
+      <div style={{display: 'inline-block'}}>
+        <div style={{display: 'inline-block',position: 'static',textalign: 'center'}}>
+          <Link to='/aranceles'>
+            <img src={ArancelDiurno} height={250} alt='Arancel'  /><br />
+            <Button>REVISA LOS ARANCELES</Button>
+          </Link>
+        </div>
+        
+        <div style={{display: 'inline-block',position: 'static',textalign: 'center' }}>
+          <Link to='/admision'>
+            <img src={Admision} height={250} alt='admi'  /><br />
+            <Button>ADMISIÓN</Button>
+          </Link>
+        </div>
+
+        <div style={{display: 'inline-block',position: 'static',textalign: 'center' }}>
+          <Link to='/simulacion_1'>
+            <img src={Simulacion} height={250} alt='aimu'  /><br />
+            <Button>SIMULA TUS PUNTAJES</Button>
+          </Link>
+        </div>
+        
+        
+        
+      </div>
+      
+      
+      
+      
+
+
+
     </div>
   )
 }
